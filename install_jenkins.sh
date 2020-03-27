5238f5eed787ba4903ed9a8909eab6712482fcf8
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-27 12:18:37 -0400

    run jenkins as ec2-user

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -5,4 +5,4 @@ sudo yum update -y
 sudo yum install -y java
 cd /home/ec2-user
 wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
-java -jar jenkins.war &
+su - ec2-user -c "java -jar jenkins.war &"
